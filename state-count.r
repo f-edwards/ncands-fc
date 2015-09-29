2cf992bca2ab9079832ab26a9d8f0eeda7f04d3a
frankalready <fedwards@uw.edu>
2015-09-29 13:53:24 -0700

    update for output, sync keys

--- state-count.r
+++ state-count.r
@@ -8,41 +8,42 @@ set.seed(1)
 source("H:/ncands-fc/ncandsfunctions.r")
 
 setwd("H:/data")
-source("H:/NCANDS-clean/ncandsread.r")
+source("H:/ncands-fc/ncandsread.r")
 
 key<-read.csv("H:/NCANDS-clean/ncandskey2012.csv", head=TRUE)
-
-### comment out full data loop, troubleshoot with one year
-# files<-c("Child2012v1.dat"
-# 	, "Child2010v1a.dat",
-# 	"Child2009v2a.dat", "Child2008v3a.dat",
-# 	"Child2007v2a.dat", "Child2006_v1a.dat",
-# 	"Child2005v1a.dat", "Child2004_v1a.dat")
-# year<-c(2012, 2010, 2009, 2008, 2007, 2006, 2005, 2004)
-# state.out<-list()
-# # z<-list()
-# # for(j in (1:length(files))){
-# # z[[j]]<-readLines(files[j],5)
-# # }
-# ### DEAL WITH VARYING FORMATS - WHEN IS DAT, WHEN NEED TO CONVERT FROM STATA
-# for(i in (1:length(files))){
-# dat<-ncands.fwf(dat=files[i], "H:/NCANDS-clean/ncandskey2012.csv")
-# dat<-ncandsclean(dat)
-# state.count<- dat %>%
-# 	group_by(st)%>%
-# 	summarise(report_child=n(),
-# 		reports=n_distinct(RptID),
-# 		rpt.police=sum('%in%'(rptsrc, "cj")),
-# 		rpt.edu=sum('%in%'(rptsrc, "education")),
-# 		rpt.med=sum('%in%'(rptsrc, "medical")),
-# 		rpt.welf=sum('%in%'(rptsrc, "socserv"))
-# 		)
-
-# state.count$year<-year[i]
-# state.out[[i]]<-state.count
-# rm(dat)
-
+files<-c("Child2012v1.dat"
+	, "Child2010v1a.dat",
+	"Child2009v2a.dat", "Child2008v3a.dat",
+	"Child2007v2a.dat", "Child2006_v1a.dat",
+	"Child2005v1a.dat", "Child2004_v1a.dat"
+	)
+year<-c(2012, 2010, 2009, 2008, 2007, 2006, 2005, 2004)
+state.out<-list()
+# z<-list()
+# for(j in (1:length(files))){
+# z[[j]]<-readLines(files[j],5)
 # }
+### DEAL WITH VARYING FORMATS - WHEN IS DAT, WHEN NEED TO CONVERT FROM STATA
+
+## current script is all children - should probably do unique reports - so first eliminate dup rptID, then count
+for(i in (1:length(files))){
+dat<-ncands.fwf(dat=files[i], "H:/ncands-fc/ncandskey2012.csv")
+dat<-ncandsclean(dat)
+state.count<- dat %>%
+	group_by(st)%>%
+	summarise(report_child=n(),
+		reports=n_distinct(RptID),
+		rpt.police=sum('%in%'(rptsrc, "cj")),
+		rpt.edu=sum('%in%'(rptsrc, "education")),
+		rpt.med=sum('%in%'(rptsrc, "medical")),
+		rpt.welf=sum('%in%'(rptsrc, "socserv"))
+		)
+
+state.count$year<-year[i]
+state.out[[i]]<-state.count
+rm(dat)
+
+}
 
 dat<-read.csv("Child2011.csv")
 state.count<- dat %>%

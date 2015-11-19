74b3098e96bb0ba544ac6666799baabe57e6b419
unknown <fedwards@uw.edu>
2015-11-18 16:53:59 -0800

    acs read add

---
+++ acs-read.r
@@ -0,0 +1,27 @@
+rm(list=ls())
+library(data.table)
+library(dplyr)
+
+setwd("H:/census")
+
+colClasses=sapply(fread("usa_0032.csv", nrows=100), class)
+colClasses[5:6]<-"character"
+dat<-fread("usa_00032.csv", sep=",", 
+	drop=c(2,3,4,8,9),
+	colClasses=colClasses)
+
+dat2<-dat%>%
+	mutate(FIPS=paste(STATEFIP, COUNTYFIPS, sep=""))%>%
+	group_by(FIPS, YEAR)%>%
+	summarise(totpop=sum(PERWT),
+		chpop=sum((age<18)*PERWT),
+		chpov=sum((age<18)*(POVERTY<150)*(PERWT)),
+		blkpop=sum((RACE==2)*PERWT),
+		amindpop=sum((RACE==3)*PERWT),
+		unemp=sum((EMPSTAT==2)*PERWT),
+		labforce=sum((EMPSTAT==1)*PERWT + (EMPSTAT==2)*PERWT),
+		kids2par=sum((POPLOC!=0)*(MOMLOC!=0)*(AGE<18)*PERWT)
+		)
+
+write.csv(dat2, "acs-county-2007-12.csv", col.names=TRUE, row.names=FALSE)
+
